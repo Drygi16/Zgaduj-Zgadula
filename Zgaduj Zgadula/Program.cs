b5481c74c33bd012890d67857184451ee6b2c04c
mac <mac@DESKTOP-LU3IG6O>
2020-11-28 18:58:57 +0100

    Add project files.

---
+++ Zgaduj Zgadula/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Zgaduj_Zgadula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rnd = new Random();
+            int los = rnd.Next(1, 11);
+
+            int mojaLiczba;
+            int ileRazy = 0;
+
+            Console.WriteLine("Zgadnij iczbę z zakresu 1-10");
+            do
+            {
+
+                ileRazy++;
+                Console.Write("Podaj liczbę: ");
+                mojaLiczba = int.Parse(Console.ReadLine());
+                if (mojaLiczba > 10 || mojaLiczba < 1)
+                {
+                    Console.WriteLine("Liczba spoza zakresu");
+                }
+                else
+                {
+
+
+                    if (mojaLiczba > los)
+                    {
+                        Console.WriteLine("Podałeś zbyt dużą liczbę!");
+                    }
+                    else if (mojaLiczba < los)
+                    {
+                        Console.WriteLine("Podałeś zbyt małą liczbę!");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Brawo odgadłeś moją liczbę!!! Za {0} razem", ileRazy);
+                    }
+                }
+            } while (los != mojaLiczba);
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
